eebbe368b54e07e9e794b6b8ea1d6a809aad4fad
Mr.fant <yii.fant@gmail.com>
2018-05-31 18:12:51 +0800

    refactor with bootstrap

--- projects/AngularJS/todoList/todo.js
+++ projects/AngularJS/todoList/todo.js
@@ -3,18 +3,28 @@ angular.module('todoApp', [])
     var todoList = this;
     todoList.todos = [
       {text:'learn AngularJS', done:true},
-      {text:'build an AngularJS app', done:false}];
+      {text:'build an AngularJS app', done:false}
+    ];
  
     todoList.addTodo = function() {
-      todoList.todos.push({text:todoList.todoText, done:false});
-      todoList.todoText = '';
+      if (todoList.todoText){
+        let text=todoList.todoText.trim();
+        if (text){
+          todoList.todos.push({text:text, done:false});
+          todoList.todoText = '';
+        }
+      }
     };
  
     todoList.remaining = function() {
       var count = 0;
-      angular.forEach(todoList.todos, function(todo) {
-        count += todo.done ? 0 : 1;
-      });
+      // angular.forEach(todoList.todos, function(todo) {
+      //   count += todo.done ? 0 : 1;
+      // });
+      for (var todo of todoList.todos){
+        if (!todo.done)
+          count++;
+      }
       return count;
     };
  
